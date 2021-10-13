366ba8dfa0ee05bee6a5498498bfb938944e023d
DerCookieLiker <68131643+DerCookieLiker@users.noreply.github.com>
2021-10-13 08:44:21 +0200

    [Feature] Design

--- js/script.js
+++ js/script.js
@@ -1,50 +1,54 @@
 "use strict";
-function ViewModel(){
-
-    this.historyArray = [];
-
-    this.render = function() {
-        document.getElementById("history").innerHTML = "";
-
-        for(let history of this.historyArray){
-            this.addHistory(history);
-        }
-    };
-    this.bind = function (btnSelector) {
-        document.querySelector(btnSelector).onclick = () => {
-            this.add();
-            this.render();
-        }
-    };
-    this.bindReset = function (btnSelector) {
-        document.querySelector(btnSelector).onclick = () => {
-            this.reset();
-            this.render();
-        }
-    };
-    this.add = function () {
-        let factor = Number(document.getElementById("factorNumber").value);
-        this.addHistoryItem(factor);
-        this.simulateRequest(factor, (factor) => {document.getElementById("product").innerHTML = (Number(document.getElementById("product").innerHTML) * factor);})
-    };
-    this.reset = function () {
-        document.getElementById("product").innerHTML = 1;
+class ViewModel {
+    
+    historyArray;
+    constructor() {
         this.historyArray = [];
-    };
-    this.addHistoryItem = function (value) {
-        if(!this.historyArray.find(x => x == value)) this.historyArray.push(value);
-    };
-    this.addHistory = function (value) {
-        let li = document.createElement("li");
-        li.innerHTML = value;
-        li.classList.add("historyItem");
-        document.getElementById("history").prepend(li);
-    };
-    this.simulateRequest = function (factor, callback) {
-        setTimeout(() => {
-            callback(factor);
-        }, 5000)
     }
+
+        render = function () {
+            document.getElementById("history").innerHTML = "";
+
+            for (let history of this.historyArray) {
+                this.addHistory(history);
+            }
+        };
+        bind = function (btnSelector) {
+            document.querySelector(btnSelector).onclick = () => {
+                this.add();
+                this.render();
+            };
+        };
+        bindReset = function (btnSelector) {
+            document.querySelector(btnSelector).onclick = () => {
+                this.reset();
+                this.render();
+            };
+        };
+        add = function () {
+            let factor = Number(document.getElementById("factorNumber").value);
+            this.addHistoryItem(factor);
+            this.simulateRequest(factor, (factor) => { document.getElementById("product").innerHTML = (Number(document.getElementById("product").innerHTML) * factor); });
+        };
+        reset = function () {
+            document.getElementById("product").innerHTML = 1;
+            this.historyArray = [];
+        };
+        addHistoryItem = function (value) {
+            if (!this.historyArray.includes(value))
+                this.historyArray.push(value);
+        };
+        addHistory = function (value) {
+            let li = document.createElement("li");
+            li.innerHTML = value;
+            li.classList.add("historyItem");
+            document.getElementById("history").prepend(li);
+        };
+        simulateRequest = function (factor, callback) {
+            setTimeout(() => {
+                callback(factor);
+            }, 5000);
+        };
 }
 
 let viewModel = new ViewModel();
