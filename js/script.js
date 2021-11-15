39aca64ca3906b71fb0aea415eb23efecd5d2913
DerCookieLiker <68131643+DerCookieLiker@users.noreply.github.com>
2021-11-15 11:41:29 +0100

    [Feature] Ajax call

--- js/script.js
+++ js/script.js
@@ -28,7 +28,18 @@ class ViewModel {
         add = function () {
             let factor = Number(document.getElementById("factorNumber").value);
             this.addHistoryItem(factor);
-            this.simulateRequest(factor, (factor) => { document.getElementById("product").innerHTML = (Number(document.getElementById("product").innerHTML) * factor); });
+            
+            this.sendRequest(
+                "GET", 
+                "https://calcservice.getsandbox.com/evaluate",
+                {
+                    operand1: factor,
+                    operator: '*',
+                    operand2: document.getElementById("product").innerHTML
+                },
+                {},
+                (factor) => { document.getElementById("product").innerHTML = (Number(document.getElementById("product").innerHTML) * factor); }
+            );
         };
         reset = function () {
             document.getElementById("product").innerHTML = 1;
@@ -49,6 +60,46 @@ class ViewModel {
                 callback(factor);
             }, 5000);
         };
+        sendRequest =  function (method, urlP, searchParams, body, callback) {
+            
+            let lookUpTable = ["GET", "PUT", "POST", "DELETE"];
+
+            if(!lookUpTable.includes(method)){
+                alert("The method: " + method + " is not valid!");
+                return;
+            }
+            
+            let request = new XMLHttpRequest();
+            request.responseType = "json";
+    
+            let url = new URL(urlP);
+
+            for (let key in searchParams) {
+                url.searchParams.set(key, searchParams[key]);
+            }
+
+            request.open(method, url);
+
+            if(method === "POST" || method === "PUT"){
+                request.setRequestHeader("Content-Type", "application/json");
+            }
+
+            request.onload = function () {
+                if(request.status === 200){
+                    callback(request.response); 
+                }else{
+                    alert(`Error ${request.status}: ${request.statusText}`);
+                }
+            }
+            if(body != String)
+                body = JSON.stringify(body);
+
+            if(method === "GET" || method === "DELETE"){
+                request.send();
+            }else{
+                request.send(body);
+            }
+        }
 }
 
 let viewModel = new ViewModel();
